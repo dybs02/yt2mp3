c2d58e054b380f0cef4f41f9649ae842b2e0221e
Mateusz Dybala <mateuszdybala2@gmail.com>
2024-07-31 15:10:51 +0200

    add cropper file load

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <FileSelector />
   <div class="mt-8 md:flex">
     <FileProperties class="md:w-2/3" />
-    <CoverEditor class="md:w-1/3" img-src="./../example.jpg" />
+    <CoverEditor class="md:w-1/3" />
   </div>
 </template>
 
--- src/components/CoverEditor.vue
+++ src/components/CoverEditor.vue
@@ -1,22 +1,32 @@
 <template>
   <div class="mt-4 md:mt-0">
     <h2 class="text-2xl font-semibold my-4">Cover Image</h2>
-
-    <div class="p-2 rounded-md bg-color">
-      <vue-cropper
-        ref="cropper"
-        :key="time"
-        :src="imgSrc"
-        :aspect-ratio="aspectRatio ? 1 : NaN"
-        alt="Source Image"
-        @ready="ready"
-      >
-      </vue-cropper>
+    <div>
+      <input
+        ref="input"
+        type="file"
+        accept="image/*"
+        @change="loadFile($event)"
+        class="w-full mb-2"
+      />
     </div>
 
-    <div class="bg-color w-fit my-2 px-2 rounded-md">
-      <input type="checkbox" id="aspect-ratio" v-model="aspectRatio" @change="refreshCropper">
-      <label for="aspect-ratio"> 1:1 Aspect Ratio</label>
+    <div v-if="isFileSelected">
+      <div class="p-2 rounded-md bg-color">
+        <vue-cropper
+          ref="cropper"
+          :src="null"
+          :aspect-ratio="aspectRatio ? 1 : NaN"
+          alt="Cover Image"
+          @ready="ready"
+        >
+        </vue-cropper>
+      </div>
+
+      <div class="bg-color w-fit my-2 px-2 rounded-md">
+        <input type="checkbox" id="aspect-ratio" v-model="aspectRatio" @change="updateAspectRatio">
+        <label for="aspect-ratio"> 1:1 Aspect Ratio</label>
+      </div>
     </div>
 
   </div>
@@ -24,28 +34,36 @@
 
 <script setup lang="ts">
 import 'cropperjs/dist/cropper.css';
-import { defineProps, ref } from 'vue';
+import { nextTick, ref } from 'vue';
 import VueCropper from 'vue-cropperjs';
 
 
-const time = ref(Date.now());
+const cropper = ref<any>(null);
+const isFileSelected = ref(false);
 const aspectRatio = ref(true);
 
-defineProps({
-  imgSrc: {
-    type: String,
-    required: true
-  }
-})
 
-
-const refreshCropper = () => {
-  console.log(aspectRatio);
-  time.value = Date.now();
+const updateAspectRatio = () => {
+  cropper.value.setAspectRatio(aspectRatio.value ? 1 : NaN);
 }
 
 const ready = () => {
   console.log('ready');
+}
+
+const loadFile = (e: Event) => {
+  const target = e.target as HTMLInputElement;
+
+  const file = target.files?.item(0);
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = async (e) => {
+      isFileSelected.value = true;
+      await nextTick()
+      cropper.value.replace(e.target?.result as string);
+    }
+    reader.readAsDataURL(file);
+  }
 }
 
 
